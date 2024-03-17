f3856f94ffd410df0e62d208accae9d2a7c74c2d
anoopraju31 <anoopraju31@gmail.com>
2024-03-17 16:50:01 +0530

    Feed component - styled

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,60 @@
-import { Box } from '@mui/material'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import ShareIcon from '@mui/icons-material/Share'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import Checkbox from '@mui/material/Checkbox'
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder'
+import Favorite from '@mui/icons-material/Favorite'
 
 export default function Feed() {
 	return (
-		<Box flex={4} p={2} bgcolor='pink'>
-			Feed
+		<Box flex={4} p={2}>
+			<Card>
+				<CardHeader
+					avatar={
+						<Avatar sx={{ bgcolor: 'red' }} aria-label='recipe'>
+							R
+						</Avatar>
+					}
+					action={
+						<IconButton aria-label='settings'>
+							<MoreVertIcon />
+						</IconButton>
+					}
+					title='Shrimp and Chorizo Paella'
+					subheader='September 14, 2016'
+				/>
+				<CardMedia
+					component='img'
+					height='20%'
+					image='https://mui.com/static/images/cards/paella.jpg'
+					alt='Paella dish'
+				/>
+				<CardContent>
+					<Typography variant='body2' color='text.secondary'>
+						This impressive paella is a perfect party dish and a fun meal to
+						cook together with your guests. Add 1 cup of frozen peas along with
+						the mussels, if you like.
+					</Typography>
+				</CardContent>
+
+				<CardActions disableSpacing>
+					<Checkbox
+						icon={<FavoriteBorder />}
+						checkedIcon={<Favorite sx={{ color: 'red' }} />}
+					/>
+					<IconButton aria-label='share'>
+						<ShareIcon />
+					</IconButton>
+				</CardActions>
+			</Card>
 		</Box>
 	)
 }
